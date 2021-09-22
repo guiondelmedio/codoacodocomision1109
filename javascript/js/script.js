44cebc34d2fab1ae52f46e279614ef96407a67c4
guiondelmedio <guiondelmedio@gmail.com>
2021-09-22 11:55:07 -0300

    Javascript inicial

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,37 +1,40 @@
 
-    <script>
-      alert("ventana emergente");
-      console.log("Texto de consola");
-      /*Variables */
-      let nombre="Mariano Devia"; /*string o cadena de caracteres */
-      var numero=18; /*numérico */
-      const PI=3.1416; /*numérico constante */
-      let hayClases=true; /*boolerano */
-      console.log((numero+25)+" "+nombre); /* concatenar y sumar */
-      console.log(hayClases);
-      let esFeriado=false;
+    alert("ventana emergente");
+    console.log("Texto de consola");
+    /*Variables */
+    let nombre="Mariano Devia"; /*string o cadena de caracteres */
+    var numero=18; /*numérico */
+    const PI=3.1416; /*numérico constante */
+    let hayClases=true; /*boolerano */
+    console.log((numero+25)+" "+nombre); /* concatenar y sumar */
+    console.log(hayClases);
+    let esFeriado=false;
       /* condicionales*/
       /* si...sino */
 
-      if (5>3){
-            console.log("5 es mayor a 3");
+      if (5 > 3){
+        console.log("5 es mayor a 3");
       } else {
-            console.log("5 no es mayor a 3");
+        console.log("5 no es mayor a 3");
       } 
 
-      console.log((5>3)? "5 es mayor a 3":"5 no es mayor a 3"); /*Ternario*/
+      console.log((5 > 3)? "5 es mayor a 3":"5 no es mayor a 3"); /*Ternario*/
 
-      if ((hayClases == true) && esFeriado != true ){
-          console.log("Me conecto a la videollamada")
-      }else{
-          console.log("no me conecto a la videollamada")
-      }
-      
-      if ( 25 < 32 || 12 > 20) { /*se cumple solo una condicióm*/
-          console.log("Se cumplió la condición")
-      }
+    if ((hayClases == true) && esFeriado != true ){
+        console.log("Me conecto a la videollamada")
+    }else{
+        console.log("no me conecto a la videollamada")
+    }
 
-      let saludo = "Hola, como estás?"
-      let saludar = saludo +" "+ nombre;
-      conole.log(saludar);
-    </script>
+    if ( 25 < 32 || 12 > 20) { /*se cumple solo una condicióm*/
+        console.log("Se cumplió la condición")
+    }
+
+    let saludo = "Hola, como estás?"
+    let saludar = saludo +" "+ nombre;
+    console.log(saludar);
+
+    let elemento=document.getElementById("principal");
+    //console.log(elemento);
+    elemento.innerHTML="<h1>"+saludar+"</h1>";
+    elemento.documentWrite("<h1 class='text-center bg-info'>"+saludar+"</h1>");
